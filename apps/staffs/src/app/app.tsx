11d8d7540aab8d89bb19cf396b815a581a6a90f3
MikaaZz <mikhael928@gmail.com>
2022-08-24 21:33:43 -0300

    Create Functions_staffs content,Role_staffs component and adding HTML content in app.tsx

--- apps/staffs/src/app/app.tsx
+++ apps/staffs/src/app/app.tsx
@@ -1,40 +1,47 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Accordion_personal, Header } from '@corre-brasil/ui';
 import './app.module.css';
+import { Functions_staffs } from './components/Functions_staffs';
 
 export const App = () => {
   return (
     <>
       <Header />
-      <Accordion_personal
-        accordionTitle={'Pista - Hidratação:'}
-        accordionContent={
-          <ul>
-            <li>A equipe irá levar você ao seu ponto;</li>
-            <li>
-              Basta entregar copo para o atleta que estiver passando e manter o
-              ponto abastecido;
-            </li>
-            <li>
-              O copo de água (em pé) deve estar na palma da sua mão, mão virada
-              para cima, braço estendido para facilitar que o atleta pegue o
-              copo;
-            </li>
-            <li>
-              Se posicionar ao lado do cocho de água e não na frente do mesmo;
-            </li>
-            <li>
-              Após a passagem dos atletas, manter seu perímetro limpo ajuntando
-              os copos que estiverem na via. Você receberá sacos de lixo e
-              vassouras para facilitar esse manejo;
-            </li>
-            <li>
-              O caminhão da organização vai passar no percurso recolhendo esse
-              lixo;
-            </li>
-          </ul>
-        }
-      />
+      <div>
+        <div>
+          <h2>
+            Corrida <br />
+            de rua no Brasil
+          </h2>
+        </div>
+        <p>
+          A Corrida de Rua movimenta, atualmente, de 4 a 10 milhões de pessoas
+          no Brasil;
+        </p>
+        <p>
+          É considerado o esporte mais democrático, pois pode ser praticado em
+          qualquer lugar, horário e clima;
+        </p>
+      </div>
+      <div>
+        <div>
+          <h3>
+            A modalidade da
+            <br /> Meia Maratona
+          </h3>
+        </div>
+        <p>
+          A meia maratona nada mais é do que uma prova de corrida que
+          corresponde a metade da distância de uma maratona, o equivalente a 21,
+          097km.
+        </p>
+        <p>
+          Essa modalidade não está inclusa em campeonatos oficiais, como as
+          Olimpíadas, porém existem inúmeras provas famosas disputadas no mundo
+          inteiro, inclusive no Brasil.
+        </p>
+      </div>
+      <Functions_staffs />
     </>
   );
 };
