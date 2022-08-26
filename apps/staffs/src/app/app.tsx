8efb060c975d27e0de477f1d4462caf6b5d4acd9
MikaaZz <mikhael928@gmail.com>
2022-08-25 21:47:07 -0300

    Create two new blocks - Title block and Mode block

--- apps/staffs/src/app/app.tsx
+++ apps/staffs/src/app/app.tsx
@@ -1,47 +1,49 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Accordion_personal, Header } from '@corre-brasil/ui';
-import './app.module.css';
-import { Functions_staffs } from './components/Functions_staffs';
+import './app.css';
+import { Functions_staffs } from './components/Functions_staffs/Functions_staffs';
 
 export const App = () => {
   return (
     <>
       <Header />
-      <div>
-        <div>
-          <h2>
-            Corrida <br />
-            de rua no Brasil
-          </h2>
+      <main className="main-content">
+        <div className="main-content__initialInfos">
+          <div className="main-content__initialInfos--titleContainer">
+            <h2 className="main-content__initialInfos__titleContainer-title">
+              Corrida <br />
+              de rua no Brasil
+            </h2>
+          </div>
+          <p className="main-content__initialInfos--textOne">
+            A Corrida de Rua movimenta, atualmente, de 4 a 10 milhões de pessoas
+            no Brasil;
+          </p>
+          <p className="main-content__initialInfos--textTwo">
+            É considerado o esporte mais democrático, pois pode ser praticado em
+            qualquer lugar, horário e clima;
+          </p>
         </div>
-        <p>
-          A Corrida de Rua movimenta, atualmente, de 4 a 10 milhões de pessoas
-          no Brasil;
-        </p>
-        <p>
-          É considerado o esporte mais democrático, pois pode ser praticado em
-          qualquer lugar, horário e clima;
-        </p>
-      </div>
-      <div>
-        <div>
-          <h3>
-            A modalidade da
-            <br /> Meia Maratona
-          </h3>
+        <div className="main-content__modeInfos">
+          <div className="main-content__modeInfos--titleContainer">
+            <h3 className="main-content__modeInfos__titleContainer-title">
+              A modalidade da
+              <br /> Meia Maratona
+            </h3>
+          </div>
+          <p className="main-content__modeInfos--textOne">
+            A meia maratona nada mais é do que uma prova de corrida que
+            corresponde a metade da distância de uma maratona, o equivalente a
+            21, 097km.
+          </p>
+          <p className="main-content__modeInfos--textTwo">
+            Essa modalidade não está inclusa em campeonatos oficiais, como as
+            Olimpíadas, porém existem inúmeras provas famosas disputadas no
+            mundo inteiro, inclusive no Brasil.
+          </p>
         </div>
-        <p>
-          A meia maratona nada mais é do que uma prova de corrida que
-          corresponde a metade da distância de uma maratona, o equivalente a 21,
-          097km.
-        </p>
-        <p>
-          Essa modalidade não está inclusa em campeonatos oficiais, como as
-          Olimpíadas, porém existem inúmeras provas famosas disputadas no mundo
-          inteiro, inclusive no Brasil.
-        </p>
-      </div>
-      <Functions_staffs />
+        <Functions_staffs />
+      </main>
     </>
   );
 };
